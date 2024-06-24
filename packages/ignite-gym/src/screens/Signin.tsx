0d0acfa0b0aa204ca66ce24b364b61086749c3f9
Josue <ozzyplacidojunior@hotmail.com>
2024-06-24 17:01:08 -0300

    feat(react-native): chapter 03 module 3 - public routes
    - fix background on screens sign and signup
    - create the routes for authentication flow

--- packages/ignite-gym/src/screens/Signin.tsx
+++ packages/ignite-gym/src/screens/Signin.tsx
@@ -5,21 +5,29 @@ import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
 export function SignIn() {
+	const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+	function handleNewAccount() {
+		navigation.navigate("signUp");
+	}
+
 	return (
 		<ScrollView
 			contentContainerStyle={{ flexGrow: 1 }}
 			showsVerticalScrollIndicator={false}
 		>
-			<VStack flex={1} bg="gray.700" px={10} pb={16}>
-				<Image
-					source={BackgroundImg}
-					alt="Pessoas treinando"
-					position="absolute"
-					resizeMode="cover"
-				/>
-
+			<Image
+				source={BackgroundImg}
+				defaultSource={BackgroundImg}
+				alt="Pessoas treinando"
+				position="absolute"
+				resizeMode="stretch"
+			/>
+			<VStack flex={1} px={10} pb={16}>
 				<Center my={24}>
 					<LogoSvg />
 					<Text color="gray.100" fontSize="sm">
@@ -56,7 +64,11 @@ export function SignIn() {
 					</Text>
 				</Center>
 
-				<Button title="Criar Conta" variant="outline" />
+				<Button
+					title="Criar Conta"
+					variant="outline"
+					onPress={handleNewAccount}
+				/>
 			</VStack>
 		</ScrollView>
 	);
--- packages/ignite-gym/src/screens/Signup.tsx
+++ packages/ignite-gym/src/screens/Signup.tsx
@@ -5,48 +5,64 @@ import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignUp() {
+	const navigation = useNavigation();
+
+	function handleGoBack() {
+		navigation.goBack();
+	}
 	return (
-		<VStack flex={1} bg="gray.700" px={10} pb={16}>
+		<ScrollView
+			contentContainerStyle={{ flexGrow: 1 }}
+			showsVerticalScrollIndicator={false}
+		>
 			<Image
 				source={BackgroundImg}
+				defaultSource={BackgroundImg}
 				alt="Pessoas treinando"
 				position="absolute"
-				resizeMode="cover"
+				resizeMode="stretch"
 			/>
+			<VStack flex={1} px={10} pb={16}>
+				<Center my={24}>
+					<LogoSvg />
 
-			<Center my={24}>
-				<LogoSvg />
+					<Text color="gray.100" fontSize="sm">
+						Treine sua mente e o seu corpo.
+					</Text>
+				</Center>
 
-				<Text color="gray.100" fontSize="sm">
-					Treine sua mente e o seu corpo.
-				</Text>
-			</Center>
+				<Center>
+					<Heading
+						color="gray.100"
+						fontSize="xl"
+						mb={6}
+						fontFamily="heading"
+					>
+						Crie sua conta
+					</Heading>
 
-			<Center>
-				<Heading
-					color="gray.100"
-					fontSize="xl"
-					mb={6}
-					fontFamily="heading"
-				>
-					Crie sua conta
-				</Heading>
+					<Input placeholder="Nome" />
 
-				<Input placeholder="Nome" />
+					<Input
+						placeholder="E-mail"
+						keyboardType="email-address"
+						autoCapitalize="none"
+					/>
+					<Input placeholder="Senha" secureTextEntry />
 
-				<Input
-					placeholder="E-mail"
-					keyboardType="email-address"
-					autoCapitalize="none"
+					<Button title="Criar e acessar" />
+				</Center>
+
+				<Button
+					title="Voltar para o login"
+					variant="outline"
+					mt={24}
+					onPress={handleGoBack}
 				/>
-				<Input placeholder="Senha" secureTextEntry />
-
-				<Button title="Criar e acessar" />
-			</Center>
-
-			<Button title="Voltar para o login" variant="outline" mt={24} />
-		</VStack>
+			</VStack>
+		</ScrollView>
 	);
 }
