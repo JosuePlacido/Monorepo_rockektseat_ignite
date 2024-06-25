859e8707da865d69a0a6ff693d9bc2b0cc869022
Josue <ozzyplacidojunior@hotmail.com>
2024-06-25 15:32:06 -0300

    feat(react-native): chapter03 module 05 - image gallery
    - implement choose image on gallery
    - validating file size

--- packages/ignite-gym/src/screens/Profile.tsx
+++ packages/ignite-gym/src/screens/Profile.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
+import * as FileSystem from "expo-file-system";
 import {
 	Center,
 	ScrollView,
@@ -7,8 +8,9 @@ import {
 	Skeleton,
 	Text,
 	Heading,
-	useTheme
+	useToast
 } from "native-base";
+import * as ImagePicker from "expo-image-picker";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -19,6 +21,44 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
 	const [photoIsLoading, setPhotoIsLoading] = useState(false);
+	const [userPhoto, setUserPhoto] = useState(
+		"https://github.com/JosuePlacido.png"
+	);
+
+	const toast = useToast();
+
+	async function handleUserPhotoSelected() {
+		setPhotoIsLoading(true);
+
+		try {
+			const photoSelected = await ImagePicker.launchImageLibraryAsync({
+				mediaTypes: ImagePicker.MediaTypeOptions.Images,
+				quality: 1,
+				aspect: [4, 4],
+				allowsEditing: true
+			});
+
+			if (photoSelected.assets) {
+				const photoInfo = await FileSystem.getInfoAsync(
+					photoSelected.assets[0].uri
+				);
+				if (photoInfo.exists) {
+					if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+						return toast.show({
+							title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+							placement: "top",
+							bgColor: "red.500"
+						});
+					}
+				}
+				setUserPhoto(photoSelected.assets[0].uri);
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setPhotoIsLoading(false);
+		}
+	}
 
 	return (
 		<VStack flex={1}>
@@ -37,14 +77,14 @@ export function Profile() {
 					) : (
 						<UserPhoto
 							source={{
-								uri: "https://github.com/JosuePlacido.png"
+								uri: userPhoto
 							}}
 							alt="Foto do usuário"
 							size={PHOTO_SIZE}
 						/>
 					)}
 
-					<TouchableOpacity>
+					<TouchableOpacity onPress={handleUserPhotoSelected}>
 						<Text
 							color="green.500"
 							fontWeight="bold"
