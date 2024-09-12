37c5df38ce666e271aff10de72eb531350a4cef2
Josue <ozzyplacidojunior@hotmail.com>
2024-09-12 18:21:27 -0300

    feat(reactjs): chapter 05 - Next Fundamentals module 04: product and checkout

--- packages/ignite-shop/src/pages/index.tsx
+++ packages/ignite-shop/src/pages/index.tsx
@@ -2,7 +2,9 @@ import 'keen-slider/keen-slider.min.css';
 
 import { useKeenSlider } from 'keen-slider/react';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import Stripe from 'stripe';
 
 import { stripe } from '@/lib/stripe';
@@ -25,20 +27,27 @@ export default function Home({ products }: HomeProps) {
 	});
 
 	return (
-		<HomeContainer ref={sliderRef} className="keen-slider">
-			{products.map(product => {
-				return (
-					<Product key={product.id} className="keen-slider__slide">
-						<Image src={product.imageUrl} width={520} height={480} alt="" />
+		<>
+			<Head>
+				<title>Home | Ignite Shop</title>
+			</Head>
+			<HomeContainer ref={sliderRef} className="keen-slider">
+				{products.map(product => {
+					return (
+						<Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+							<Product className="keen-slider__slide">
+								<Image src={product.imageUrl} width={520} height={480} alt="" />
 
-						<footer>
-							<strong>{product.name}</strong>
-							<span>{product.price}</span>
-						</footer>
-					</Product>
-				);
-			})}
-		</HomeContainer>
+								<footer>
+									<strong>{product.name}</strong>
+									<span>{product.price}</span>
+								</footer>
+							</Product>
+						</Link>
+					);
+				})}
+			</HomeContainer>
+		</>
 	);
 }
 
