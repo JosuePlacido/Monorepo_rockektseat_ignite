b51db57e9431da83edfc137c619dcd9e6382f19d
Josue <ozzyplacidojunior@hotmail.com>
2024-12-15 23:05:45 -0300

    feat(reactjs): chapter 07 - Deeping in Next.js, Module 03 - Connect with Calendar
    - create prisma adapter
    - create auth routes
    - create page connect calendar
    - add authentication context

--- packages/ignite-call-app/src/pages/register/index.page.tsx
+++ packages/ignite-call-app/src/pages/register/index.page.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react';
-import { AxiosError } from 'axios';
+import axios from 'axios';
 import { useRouter } from 'next/router';
 import { ArrowRight } from 'phosphor-react';
 import { useEffect } from 'react';
@@ -44,8 +44,9 @@ export default function Register() {
 				name: data.name,
 				username: data.username
 			});
+			await router.push('/register/connect-calendar');
 		} catch (err) {
-			if (err instanceof AxiosError && err?.response?.data?.message) {
+			if (axios.isAxiosError(err) && err?.response?.data?.message) {
 				alert(err.response.data.message);
 				return;
 			}
